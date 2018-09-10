081a568b7edeffe337acdd1ec354ec0f32b2e717
Carl Björknäs <carl.bjorknas@knowit.se>
2018-09-10 12:50:25 +0200

    Adds routing to Welcome and Product list views

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -2,7 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
+  //templateUrl: './app.component.html',
+  template:`
+    <ul class='nav nav-pills'>
+      <li><a [routerLink]="['/welcome']">Home</a></li>
+      <li><a [routerLink]="['/products']">Product list</a></li>
+    </ul>
+    <router-outlet></router-outlet>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
