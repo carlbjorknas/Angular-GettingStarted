cc8e2f95ed816b1040fb882a80735497ad8641a9
Carl Björknäs <carl.bjorknas@knowit.se>
2018-09-11 12:47:29 +0200

    Adds a Shared Module. Command: ng g m shared/shared --flat -m products/product.module

--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -1,29 +1,24 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
 import { ProductListComponent } from './product-list.component';
-import { StarComponent } from '../shared/star.component';
-import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
 import { ProductDetailComponent } from './product-detail.component';
 import { RouterModule } from '@angular/router';
 import { ProductDetailGuard } from './product-detail.guard';
-import { FormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  imports: [
-    CommonModule,   
-    FormsModule, 
+  imports: [    
     RouterModule.forChild([
       { path: 'products', component: ProductListComponent},
       { path: 'products/:id', 
         component: ProductDetailComponent,
         canActivate: [ProductDetailGuard]
       }
-    ]),
+    ]), 
+    SharedModule,
   ],
   declarations: [
-    ProductListComponent,
-    StarComponent,
-    ConvertToSpacesPipe,
+    ProductListComponent,    
     ProductDetailComponent,
   ]
 })
